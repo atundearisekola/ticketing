dc5df845b85a9e551485f7f3d4b5f06c93346cb3
atundearisekola <atundearisekola@yahoo.com>
2020-09-05 08:10:15 +0100

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import buildClient from '../api/build-client'
+
+const LandingPage = ({currentUser})=>{
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
+
+}
+
+LandingPage.getInitialProps =(context)=>{
+  
+  const {data} = await buildClient(context).get('/api/users/currentUser');
+  return data;
+}
+
+export default LandingPage
